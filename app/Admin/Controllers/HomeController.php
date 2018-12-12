5a816f414b4149738ea853a82f59622d48becd8b
xiangqian <175023117@qq.com>
2018-12-12 16:34:32 +0800

    修改Dashboard

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -32,9 +32,15 @@ class HomeController extends Controller
                 });
 
                 $countUser = User::count();
-                $countDepartment = Department::count();
-                $row->column(4, new InfoBox('员工数量', 'users', 'aqua', '/admin/users', "$countUser"));
-                $row->column(4, new InfoBox('部门数量', 'book', 'red', '/admin/users', "$countDepartment"));
+                $countUserByType1 = User::where('type', 1)->count();
+                $countUserByType2 = User::where('type', 2)->count();
+                $countUserByType3 = User::where('type', 3)->count();
+//                $countDepartment = Department::count();
+                $row->column(4, new InfoBox('在职员工数量', 'users', 'aqua', '/admin/users', $countUser));
+                $row->column(4, new InfoBox('全职员工数量', 'users', 'aqua', '/admin/users?type=1', $countUserByType1));
+                $row->column(4, new InfoBox('兼职员工数量', 'users', 'aqua', '/admin/users?type=2', $countUserByType2));
+                $row->column(4, new InfoBox('实习员工数量', 'users', 'aqua', '/admin/users?type=3', $countUserByType3));
+//                $row->column(4, new InfoBox('部门数量', 'book', 'red', '/admin/users', $countDepartment));
 //                $row->column(4, new InfoBox('New Users', 'shopping-cart', 'aqua', '/demo/users', '1024'));
 
             });
